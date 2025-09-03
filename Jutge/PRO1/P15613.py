2d754d61cb6bbe92a46b34163286d57c22e13bbf
Miquel Oliver <miqueloliver10@gmail.com>
2025-09-03 18:05:44 +0200

    3/9/2025

---
+++ Jutge/PRO1/P15613.py
@@ -0,0 +1,20 @@
+temperature = int(input())
+
+if temperature >= 31:
+    print("fa calor")
+    if temperature >= 100:
+        print("l'aigua bulliria")
+elif temperature <= 9:
+    print("fa fred")
+    if temperature <= 0:
+        print("l'aigua es gelaria")
+else:
+    print("s'esta be")
+
+    
+
+    
+
+
+
+
---
+++ Jutge/PRO1/P98960.py
@@ -0,0 +1,6 @@
+letter = input()
+
+if letter.islower():
+    print(letter.upper())
+else:
+   print(letter.lower())
